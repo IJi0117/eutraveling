b72579449160728666f160b098783e1c2b47a432
IJi0117 <74041203+IJi0117@users.noreply.github.com>
2020-11-21 00:05:09 +0900

    Add files via upload

--- style2.js
+++ style2.js
@@ -1,17 +1,20 @@
 
         var target = {
           setcolor : function(color){
-            document.querySelector('body').style.color = color;},
+              $('body').css('color', color); },
+          // document.querySelector('body').style.color = color;},
           setBackgroundColor : function(color){
-            document.querySelector('body').style.backgroundColor = color;}
-        };
+          // document.querySelector('body').style.backgroundColor = color;}
+             $('body').css('background-color', color);}
+      };
         var Links = {
           setcolora : function(color){
-                      var alist = document.querySelectorAll('a');
-                      var i = 0;
-                      while (i < alist.length) {
-                        alist[i].style.color = color;
-                        i = i+1;}
+                      // var alist = document.querySelectorAll('a');
+                      // var i = 0;
+                      // while (i < alist.length) {
+                      //   alist[i].style.color = color;
+                      //   i = i+1;}
+                      $('a').css('color', color);
           }
         };
 
